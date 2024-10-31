853d8f6a61d589c9b94c782a91fea75cae8456a3
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-31 16:29:55 +0900

    reset db (after lint)

--- src/db/sql/common.sql
+++ src/db/sql/common.sql
@@ -1,5 +1,7 @@
+USE test;
 -- DROP TABLE IF EXISTS posts;
 -- DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS migrations;
 
 -- TRUNCATE TABLE posts;
-DELETE FROM users; --  Cannot truncate a table referenced in a foreign key constraint
+-- DELETE FROM users; --  Cannot truncate a table referenced in a foreign key constraint
