364b22c2e5390e7376c44b77c12be4487b5b6e9c
ddubbu-dev <dev.ddubbu@gmail.com>
2024-10-31 15:40:13 +0900

    docs: Add swagger auto-gen script

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,17 @@ import express, { Express } from 'express'
 import cookieParser from 'cookie-parser'
 import routes from './routes'
 import { AppDataSource } from './db'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './docs/swagger-output.json'
 
 const app: Express = express()
-const port = 8000
+const port = process.env.PORT
 
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use('/', routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 AppDataSource.initialize()
     .then(() => {
