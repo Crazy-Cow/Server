5fb9231572d9a652923985f50e93577afad93f6e
ddubbu-dev <hello.ddubbu@gmail.com>
2024-11-23 16:27:49 +0900

    feat: 캐릭터 단색처리  [CD-249]

--- src/game/objects/player.ts
+++ src/game/objects/player.ts
@@ -25,27 +25,24 @@ export class Character {
         id,
         nickName,
         position,
+        color,
     }: {
         id: string
         nickName: string
         position: Position
+        color: string
     }) {
         this.id = id
         this.nickName = nickName
         this.position = position
-        this.bodyColor = this.generateRandomHexColor()
-        this.hairColor = this.generateRandomHexColor()
-        this.bellyColor = this.generateRandomHexColor()
+        this.bodyColor = color
+        this.hairColor = color
+        this.bellyColor = 'white'
         this.velocity = { x: 0, y: 0, z: 0 }
         this.isOnGround = true
         this.hasTail = false
         this.shift = false
         this.isBeingStolen = false
-    }
-
-    private generateRandomHexColor(): string {
-        const color = Math.floor(Math.random() * 16777215).toString(16)
-        return '#' + color.padStart(6, '0')
     }
 
     setHasTail(hasTail: boolean) {
