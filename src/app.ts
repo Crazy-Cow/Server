0fb683ce6d9d8d79d20e14f7b9e7c329c0d36fd2
ddubbu-dev <hello.ddubbu@gmail.com>
2024-11-25 14:42:15 +0900

    feat: Connect mongodb

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ import cors, { CorsOptions } from 'cors'
 import express, { Express } from 'express'
 import cookieParser from 'cookie-parser'
 import routes from './routes'
-// import { connectDB } from './schemas'
 import { initSocket } from './socket'
 import { Server } from 'socket.io'
+import { connectDB } from './db/mongoose/schemas'
 
 const port = process.env.PORT
 const corsOption: CorsOptions = { origin: '*' }
@@ -16,29 +16,18 @@ const app: Express = express()
 app.use(cors(corsOption))
 app.use(cookieParser())
 app.use(express.json())
-app.use('/example', routes.example)
 app.use('/user', routes.user)
 
-const server = app.listen(port, () => {
-    console.log(`[2] Server runs at <http://localhost>:${port}`)
-})
+connectDB()
+    .then(() => console.log('[1] DB Connected'))
+    .then(() => {
+        const server = app.listen(port, () => {
+            console.log(`[2] Server runs at <http://localhost>:${port}`)
+        })
 
-const io = new Server(server, { cors: corsOption })
-
-initSocket(io)
-
-// connectDB()
-//     .then(() => console.log('[1] DB Connected'))
-//     .then(() => {
-//         console.log('[1] (mongoDB) DB Connected')
-//         const server = app.listen(port, () => {
-//             console.log(`[2] Server runs at <http://localhost>:${port}`)
-//         })
-
-//         const io = new Server(server, socketCorsOption)
-//         initSocket(io)
-//         initInGmaeSocket(io)
-//     })
-//     .catch((err) => {
-//         console.error('[Error] DB Connection Failed:', err)
-//     })
+        const io = new Server(server, { cors: corsOption })
+        initSocket(io)
+    })
+    .catch((err) => {
+        console.error('[Error] Server runs failed:', err)
+    })
