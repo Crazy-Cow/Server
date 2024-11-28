03a8a3e6cd39eff93016693100ba4f2fd5de0091
ddubbu-dev <hello.ddubbu@gmail.com>
2024-11-29 00:49:08 +0900

    feat(log): combo 따로 저장 [CD-354]

--- src/db/redis/respository/log/combo-steal.ts
+++ src/db/redis/respository/log/combo-steal.ts
@@ -2,14 +2,39 @@ import { redisClient } from '../..'
 import { LogCategory } from './index.type'
 import { getLogGRCUKey } from './index.util'
 
-const category: LogCategory = 'combo-steal'
+async function saveCombo(props: {
+    roomId: string
+    userId: string
+    comboCnt: number
+}) {
+    const { userId, comboCnt } = props
+    const doubleKey = getLogGRCUKey({
+        ...props,
+        category: 'combo-steal-double',
+    })
 
+    if (comboCnt == 3) {
+        await redisClient.hIncrBy(doubleKey, userId, -1)
+        const tripleKey = getLogGRCUKey({
+            ...props,
+            category: 'combo-steal-triple',
+        })
+        await redisClient.hIncrBy(tripleKey, userId, 1)
+    } else if (comboCnt == 2) {
+        await redisClient.hIncrBy(doubleKey, userId, 1)
+    }
+}
+
+const notSaveCategory: LogCategory = 'combo-steal-tmp'
 const COMBO_EXPIRE_SEC = 10
 
 async function acquireCombo(props: { roomId: string; userId: string }) {
-    const comboKey = getLogGRCUKey({ ...props, category })
+    const notSaveComboKey = getLogGRCUKey({
+        ...props,
+        category: notSaveCategory,
+    })
 
-    const comboCntStr = await redisClient.get(comboKey)
+    const comboCntStr = await redisClient.get(notSaveComboKey)
     let comboCnt = 0
 
     if (comboCntStr) {
@@ -18,13 +43,13 @@ async function acquireCombo(props: { roomId: string; userId: string }) {
         comboCnt = 1
     }
 
-    await redisClient.set(comboKey, comboCnt, { EX: COMBO_EXPIRE_SEC })
-
+    await saveCombo({ ...props, comboCnt })
+    await redisClient.set(notSaveComboKey, comboCnt, { EX: COMBO_EXPIRE_SEC })
     return { comboCnt }
 }
 
 async function resetCombo(props: { roomId: string; userId: string }) {
-    const comboKey = getLogGRCUKey({ ...props, category })
+    const comboKey = getLogGRCUKey({ ...props, category: notSaveCategory })
     await redisClient.del(comboKey)
 
     return
--- src/db/redis/respository/log/index.type.ts
+++ src/db/redis/respository/log/index.type.ts
@@ -1,6 +1,11 @@
 import { StealComboType } from 'socket/types/emit'
 
-export type LogCategory = 'event' | 'steal' | 'combo-steal'
+export type LogCategory =
+    | 'event'
+    | 'steal'
+    | 'combo-steal-double'
+    | 'combo-steal-triple'
+    | 'combo-steal-tmp'
 
 export type CommonLogProps = {
     roomId: string
