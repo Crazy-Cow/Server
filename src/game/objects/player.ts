85e235ff40f524615591676c7c82ec2e352d8f4f
seven05 <ock1997@gmail.com>
2024-12-02 16:09:27 +0900

    Fix: spqwn point and Maxspeed bug

--- src/game/objects/player.ts
+++ src/game/objects/player.ts
@@ -99,7 +99,7 @@ export abstract class Character {
     abstract getMaxSpeed(): number
 
     isValidVelocity(velocity: Position): boolean {
-        const maxSpeed = this.getMaxSpeed()
+        const maxSpeed = this.getMaxSpeed() + 1
         const speed = Math.sqrt(velocity.x ** 2 + velocity.z ** 2)
         return speed <= maxSpeed && velocity.y <= 10 && velocity.y >= -40
     }
