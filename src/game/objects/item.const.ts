112203b67470f4225df9fb94b2e7a534bf658ec4
seven05 <ock1997@gmail.com>
2024-12-03 14:42:39 +0900

    Feature: giftCnt speed server

--- src/game/objects/item.const.ts
+++ src/game/objects/item.const.ts
@@ -6,6 +6,7 @@ const TOTAL_PROB = BOOST_PROB + SHIELD_PROB + THUNDER_PROB + GIFT_PROB
 const ITEM_PICKUP_DISTANCE = 5
 const SPEED_UP = 4
 const ITEM_HEIGHT = 5
+const SPEED_DECREASE_FACTOR = 1
 
 export const ITEM = {
     BOOST_PROB,
@@ -16,6 +17,7 @@ export const ITEM = {
     ITEM_PICKUP_DISTANCE,
     SPEED_UP,
     ITEM_HEIGHT,
+    SPEED_DECREASE_FACTOR,
 }
 
 export default ITEM
