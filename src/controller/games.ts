99ceb6812581d82c6708e8c6a54655f53a251819
ddubbu-dev <hello.ddubbu@gmail.com>
2024-12-06 14:49:24 +0900

    chore: Del deprecated APIs [CD-453]

--- src/controller/games.ts
+++ src/controller/games.ts
@@ -1,71 +1,10 @@
 import { Response } from 'express'
 import {
-    GetGamePersonalSummaryRequest,
-    GetGamePersonalSummaryResponse,
     GetGameTotalRankSummaryRequest,
     GetGameTotalRankSummaryResponse,
-    GetGameTotalSummaryResponse,
 } from './games.type'
 import { createError, ErrorResponse } from '../utils/error'
 import gameSummaryService from '../service/game-summary'
-
-export const getGamePersonalSummaryController = async (
-    req,
-    res: Response<GetGamePersonalSummaryResponse | ErrorResponse>
-) => {
-    const { roomId, userId } = req.query as GetGamePersonalSummaryRequest
-
-    if (!roomId || !userId) {
-        res.status(400).json(createError({ msg: '[roomId | userId] required' }))
-        return
-    }
-
-    const player = gameSummaryService.getPlayerInfo(roomId, userId)
-    if (!player) {
-        res.status(400).json(createError({ msg: 'no player data' }))
-        return
-    }
-
-    const summary = await gameSummaryService.getPersonalSummary(roomId, userId)
-    if (!summary) {
-        res.status(404).json(createError({ msg: 'no game data' }))
-        return
-    }
-    const badges = await gameSummaryService.getPersonalBadges(roomId, userId)
-
-    const response: GetGamePersonalSummaryResponse = {
-        character: {
-            id: player.id,
-            nickName: player.nickName,
-            charType: player.charType,
-            charColor: player.charColor,
-        },
-        badges,
-        summary,
-    }
-
-    res.json(response)
-}
-
-export const getGameTotalSummaryController = async (
-    req,
-    res: Response<GetGameTotalSummaryResponse | ErrorResponse>
-) => {
-    const { roomId } = req.query as GetGamePersonalSummaryRequest
-
-    if (!roomId) {
-        res.status(400).json(createError({ msg: '[roomId] required' }))
-        return
-    }
-
-    const result = await gameSummaryService.getTotalSummary(roomId)
-    if (!result) {
-        res.status(400).json(createError({ msg: 'no player data' }))
-        return
-    }
-
-    res.json(result)
-}
 
 export const getGameTotalRankSummaryController = async (
     req,
--- src/controller/games.type.ts
+++ src/controller/games.type.ts
@@ -1,27 +1,5 @@
 import { Character } from 'game/objects/player'
 
-export type GetGamePersonalSummaryRequest = {
-    roomId: string
-    userId: string
-}
-
-export type GetGamePersonalSummaryResponse = {
-    character: Pick<Character, 'id' | 'charType' | 'charColor' | 'nickName'>
-    badges: Array<{ label: string; img: string }>
-    summary: Array<{ label: string; value: number }>
-}
-
-// (시작) deprecated
-export type GetGameTotalSummaryRequest = {
-    roomId: string
-}
-
-export type GetGameTotalSummaryResponse = {
-    character: Pick<Character, 'id' | 'charType' | 'charColor' | 'nickName'>
-    summary: Array<{ label: string; value: number }>
-}
-// (끝) deprecated
-
 export type GetGameTotalRankSummaryRequest = {
     roomId: string
 }
